3ae23eb71418ff8a08cfeb1d2869d9b910a60d59
Deyvis <deyvisjaime17@gmail.com>
2021-03-27 14:45:17 -0500

    modificando index.php a empresa.php

--- vistas/contactanos.php
+++ vistas/contactanos.php
@@ -17,14 +17,14 @@ include_once 'layouts/head.php';
 			<h4 id="categoria">Contáctanos</h4>
 		</section>
 
-		<main class="main__contacto">
+		<main class="main__contacto col-md-6">
 			<form action="../controladores/enviarcorreo.php" class="form" method="post">
 				<input class="form__section sombra" type="text" name="nombre" id="nombre" placeholder="Nombres y Apellidos" pattern="[a-zA-Z ]{2,254}" title="Solo debe contener letras. e.g. john" required>
 				<div class="form__section form__content">
-					<input type="text" class="formSec__item margin" placeholder="DNI" name="dni" id="dni" pattern="[0-9]{8}" title="Debe poner 8 números" required>
-					<input class="formSec__item" type="text" placeholder="Provincia/Distrito" name="provincia" id="provincia" required>
-					<input type="email" class="formSec__item margin" name="email" id="email" placeholder="Email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" title="e.g. aso@gmail.com" required>
-					<input class="formSec__item" type="text" name="telefono" id="telefono" placeholder="Teléfono"   pattern="[0-9]{9,12}" title="debe conter numeros" required>
+					<input type="text" class="formSec__item margin col-md-12" placeholder="DNI" name="dni" id="dni" pattern="[0-9]{8}" title="Debe poner 8 números" required>
+					<input class="formSec__item col-md-12" type="text" placeholder="Provincia/Distrito" name="provincia" id="provincia" required>
+					<input type="email" class="formSec__item margin col-md-12 " name="email" id="email" placeholder="Email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" title="e.g. aso@gmail.com" required>
+					<input class="formSec__item col-md-12" type="text" name="telefono" id="telefono" placeholder="Teléfono"   pattern="[0-9]{9,12}" title="debe conter numeros" required>
 				</div>
 				<textarea class="form__section sombra" name="comentarios"  id="comentarios" cols="70" rows="10" style="height: 64px;" placeholder="Escribe tu comentario aquí" required></textarea>
 
--- vistas/panel_usuario/index.php
+++ vistas/panel_usuario/index.php
@@ -7,7 +7,31 @@ include 'header.php';
     <?php include 'menu.html'; ?>
     <main class="main">
       <?php include 'superior.php'; ?>
-      <section class="contenido"></section>
+      <section class="container col-md-12">
+        
+          <table class="table table-hover table-Light table-responsive">
+            <thead class="bg-info text-white">
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Producto</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Precio</th>
+                <th scope="col">Estado</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td class="w-25"><img class="w-100" src="../panel_usuario/image/usuario.jpg" alt=""></td>
+                <td>Camisa Floreada<br>Estilo mancora 1kg</td>
+                <td>S/45</td>
+                <td>Vendido: 45</td>
+              </tr>
+              
+            </tbody>
+        </table>
+       
+      </section>
     </main>
   </div>
 <?php include 'footer.php'; ?>
