4050d1ffc7ad9ea4f3c5e3c4c3814dd8422a5731
Roger <2017141043@unh.edu.pe>
2021-03-30 09:02:53 -0500

    add go your business

--- vistas/panel_usuario/superior.php
+++ vistas/panel_usuario/superior.php
@@ -2,7 +2,7 @@
     <div class="menu-top">
     <img class="menuHo__img" src="image/usuario.jpg" alt="usuario" /> 
     <h2>BIENVENIDO A TU TIENDA VIRTUAL</h2>
-    <a class="ver__tienda1" href="#"><i class="fas fa-list-alt"></i> Ver tienda</a>
+    <a id="mybusiness"class="ver__tienda1" href="#"><i class="fas fa-list-alt"></i> Ver tienda</a>
     </div>
     <div class="menuHo__usuarioSection">
       <input type="text" id="rucSuperior" value="<?php echo $_SESSION['empresa']['RucEmpresa'] ?>" hidden>
