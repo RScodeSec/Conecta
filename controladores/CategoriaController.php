5e120c0e4135b8d5094e6580a67faad21f14e3d6
Deyvis <deyvisjaime17@gmail.com>
2021-04-07 17:47:59 -0500

    categoras webp

--- controladores/CategoriaController.php
+++ controladores/CategoriaController.php
@@ -23,7 +23,7 @@ class CategoriaController {
                 $no_permitidas= array ("á","é","í","ó","ú","Á","É","Í","Ó","Ú","ñ","À","Ã","Ì","Ò","Ù","Ã™","Ã ","Ã¨","Ã¬","Ã²","Ã¹","ç","Ç","Ã¢","ê","Ã®","Ã´","Ã»","Ã‚","ÃŠ","ÃŽ","Ã”","Ã›","ü","Ã¶","Ã–","Ã¯","Ã¤","«","Ò","Ã","Ã„","Ã‹");
                 $permitidas= array ("a","e","i","o","u","A","E","I","O","U","n","N","A","E","I","O","U","a","e","i","o","u","c","C","a","e","i","o","u","A","E","I","O","U","u","o","O","i","a","e","U","I","A","E","__");
                 $nomImg = str_replace($no_permitidas,$permitidas,$nomCat);
-                $urlImg = "../public/imagenes/categorias/".$nomImg.'.png';
+                $urlImg = "../public/imagenes/categorias/".$nomImg.'.webp';
                 $idCat = $cat['IdCategoria'];
                 $options .= "<div class='cuadros'>
                                 <input type='text' value='{$idCat}' hidden>
