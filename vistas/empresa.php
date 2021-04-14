c6db432301911f89cffdc31b04f5d5825dd735ec
Roger <2017141043@unh.edu.pe>
2021-04-14 13:16:58 -0500

    modified script index

--- vistas/empresa.php
+++ vistas/empresa.php
@@ -254,9 +254,17 @@ include_once 'layouts/head.php'; ?>
 
 	</div>
 
-	<?php include_once 'layouts/scripts.php'; ?>
-	<script type="text/javascript" src="../public/js/form-product.js"></script>
+	<!--</?php include_once 'layouts/scripts.php'; ?>-->
+	<script src="../public/plugins/jquery-3.5.1.min.js"></script>
 	<script src="../public/js/empresa.js"></script>
+	<script type="text/javascript" src="../public/js/form-product.js"></script>
+	<script src="../public/plugins/sweetalert2.all.min.js"></script>
+	<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>-->
+	<script type="text/javascript" src="../public/js/menu.js"></script>
+	<script type="text/javascript" src="../public/js/form_registro.js"></script>
+	<script type="text/javascript" src="../public/js/login.js"></script>
+	<!--<script type="text/javascript" src="../public/js/search.js"></script>-->
+	<script src="https://kit.fontawesome.com/c702fce202.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- vistas/index.php
+++ vistas/index.php
@@ -380,11 +380,12 @@ include_once 'layouts/head.php';
 
 	<script src="../public/plugins/jquery-3.5.1.min.js"></script>
 	<script src="../public/js/index.js"></script>
-	<script src="../public/plugins/sweetalert2.all.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
 	<script type="text/javascript" src="../public/js/menu.js"></script>
+	<script src="../public/plugins/sweetalert2.all.min.js"></script>
 	<script type="text/javascript" src="../public/js/form_registro.js"></script>
 	<script type="text/javascript" src="../public/js/login.js"></script>
+
 	<script src="https://kit.fontawesome.com/c702fce202.js" crossorigin="anonymous"></script>
 	
 
